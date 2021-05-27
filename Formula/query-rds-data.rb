392d82fc309da54177afb8457bd2335b9c1fdb93
Bruce Adams <bruce.adams@acm.org>
2021-05-27 19:54:07 -0400

    Update query-rds-data to 1.1.0

--- Formula/query-rds-data.rb
+++ Formula/query-rds-data.rb
@@ -3,8 +3,8 @@
 class QueryRdsData < Formula
   desc 'Query AWS RDS Data from the command line'
   homepage 'https://github.com/bruceadams/query-rds-data'
-  url 'https://github.com/bruceadams/query-rds-data/releases/download/1.0.0/query-rds-data.macos.universal'
-  sha256 '78ec8c4fb226d46cd4ca85f929bc11eaa2a5a1b33b965e0d99c4f64303dfb4bd'
+  url 'https://github.com/bruceadams/query-rds-data/releases/download/1.1.0/query-rds-data.macos.universal'
+  sha256 'a498064c2293c823b8785927efbcd2c4bf62e6434ab214c0d2d48f416d5732f6'
 
   def install
     bin.install 'query-rds-data.macos.universal' => 'query-rds-data'
