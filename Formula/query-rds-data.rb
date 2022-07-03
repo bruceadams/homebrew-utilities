9969716982a6da8745c37c117b1399ffd7d93224
Bruce Adams <bruce.adams@acm.org>
2022-07-03 10:55:59 -0400

    Update yj to 1.2.2 & query-rds-data to 2.0.1

--- Formula/query-rds-data.rb
+++ Formula/query-rds-data.rb
@@ -3,8 +3,8 @@
 class QueryRdsData < Formula
   desc 'Query AWS RDS Data from the command line'
   homepage 'https://github.com/bruceadams/query-rds-data'
-  url 'https://github.com/bruceadams/query-rds-data/releases/download/1.2.3/query-rds-data.macos.universal'
-  sha256 'e9ec508bf19fcf87b89d9490c3ba3da37bb5734d275f9496a2a5d4f497b5dbd5'
+  url 'https://github.com/bruceadams/query-rds-data/releases/download/2.0.1/query-rds-data.macos.universal'
+  sha256 '7b3d9d94e99270e6e15b79a5907d4b1163a4ef584e72b3d2d582163fde1ce1de'
 
   def install
     bin.install 'query-rds-data.macos.universal' => 'query-rds-data'
--- Formula/yj.rb
+++ Formula/yj.rb
@@ -3,8 +3,8 @@
 class Yj < Formula
   desc 'Command line tool that converts YAML to JSON '
   homepage 'https://github.com/bruceadams/yj'
-  url 'https://github.com/bruceadams/yj/releases/download/1.2.0/yj.macos.universal'
-  sha256 '5c24292f2f0bafc8c6e4e7d584a229bcbc6f9bea7de5bb118f15f3197710acb2'
+  url 'https://github.com/bruceadams/yj/releases/download/1.2.2/yj.macos.universal'
+  sha256 '238d03a2e0be870dfd814a153026a412402d1f2b30161f9364ceed656b6472d4'
 
   def install
     bin.install 'yj.macos.universal' => 'yj'
